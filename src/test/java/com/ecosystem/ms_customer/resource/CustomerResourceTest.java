2c54651f524aede0c8e7b956fcef9e8bdbde77ea
Lucas Carmassi <carmassilucas@gmail.com>
2025-03-20 14:42:48 -0300

    test: testing customer update

--- src/test/java/com/ecosystem/ms_customer/resource/CustomerResourceTest.java
+++ src/test/java/com/ecosystem/ms_customer/resource/CustomerResourceTest.java
@@ -3,10 +3,13 @@ package com.ecosystem.ms_customer.resource;
 import com.ecosystem.ms_customer.config.DynamoDbTestConfig;
 import com.ecosystem.ms_customer.entity.Customer;
 import com.ecosystem.ms_customer.resource.dto.CreateCustomer;
+import com.ecosystem.ms_customer.resource.dto.CustomerProfile;
+import com.ecosystem.ms_customer.resource.dto.UpdateCustomer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.awspring.cloud.dynamodb.DynamoDbTemplate;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -109,9 +112,63 @@ public class CustomerResourceTest {
         ).andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 
+    @Test
+    @DisplayName("Should be possible update customer data")
+    void should_be_possible_update_customer_data() throws Exception {
+        var body = new UpdateCustomer(
+                "updated name",
+                "updated description",
+                LocalDate.now().minusYears(30)
+        );
+
+        var customer = Customer.fromCreateCustomer(new CreateCustomer(
+                "email@email.com",
+                "secretpassword",
+                "name",
+                null,
+                null,
+                LocalDate.now().minusYears(18)
+        ));
+
+        this.dynamoDb.save(customer);
+
+        this.mvc.perform(MockMvcRequestBuilders.put("/v1/customers/email@email.com")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJSON(body))
+        ).andExpect(MockMvcResultMatchers.status().isNoContent());
+
+        var response = this.mvc.perform(MockMvcRequestBuilders.get("/v1/customers/email@email.com/profile")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse().getContentAsString();
+
+        var profile = fromJSON(response);
+
+        Assertions.assertEquals(body.name(), profile.name());
+    }
+
+    @Test
+    @DisplayName("Should not be possible update customer when customer not found")
+    void should_not_be_possible_update_customer_when_customer_not_found() throws Exception {
+        var body = new UpdateCustomer(
+                "updated name",
+                "updated description",
+                LocalDate.now().minusYears(30)
+        );
+
+        this.mvc.perform(MockMvcRequestBuilders.put("/v1/customers/email@email.com")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJSON(body))
+        ).andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
     private static String toJSON(Object object) throws JsonProcessingException {
         final ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper.writeValueAsString(object);
+    }
+
+    public static CustomerProfile fromJSON(String json) throws JsonProcessingException {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, CustomerProfile.class);
     }
 }
--- src/test/java/com/ecosystem/ms_customer/service/CustomerServiceTest.java
+++ src/test/java/com/ecosystem/ms_customer/service/CustomerServiceTest.java
@@ -5,6 +5,7 @@ import com.ecosystem.ms_customer.exception.CustomerAlreadyExistsException;
 import com.ecosystem.ms_customer.exception.CustomerNotFoundException;
 import com.ecosystem.ms_customer.exception.MinorException;
 import com.ecosystem.ms_customer.resource.dto.CreateCustomer;
+import com.ecosystem.ms_customer.resource.dto.UpdateCustomer;
 import io.awspring.cloud.dynamodb.DynamoDbTemplate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -31,9 +32,9 @@ public class CustomerServiceTest {
     @DisplayName("Should be possible create customer.")
     void should_be_possible_create_customer() {
         var body = new CreateCustomer(
-                "name",
                 "email@email.com",
                 "secretpassword",
+                "name",
                 null,
                 null,
                 LocalDate.now().minusYears(18)
@@ -48,9 +49,9 @@ public class CustomerServiceTest {
     @DisplayName("Should not be possible create customer with already registered email.")
     void should_not_be_possible_create_customer_with_already_registered_email() {
         var body = new CreateCustomer(
-                "name",
                 "email@email.com",
                 "secretpassword",
+                "name",
                 null,
                 null,
                 LocalDate.now().minusYears(18)
@@ -65,9 +66,9 @@ public class CustomerServiceTest {
     @DisplayName("Should not be possible create minor customer.")
     void should_not_be_possible_create_minor_customer() {
         var body = new CreateCustomer(
-                "name",
                 "email@email.com",
                 "secretpassword",
+                "name",
                 null,
                 null,
                 LocalDate.now().minusYears(16)
@@ -79,7 +80,7 @@ public class CustomerServiceTest {
     @Test
     @DisplayName("Should be possible get profile customer")
     void should_be_possible_get_profile_customer() {
-        String email = "email@email.com";
+        var email = "email@email.com";
 
         Mockito.when(this.dynamoDb.load(Key.builder().partitionValue(email).build(), Customer.class)).thenReturn(new Customer());
 
@@ -89,10 +90,44 @@ public class CustomerServiceTest {
     @Test
     @DisplayName("Should not be possible get profile customer when customer not found")
     void should_not_be_possible_get_profile_customer_when_customer_not_found() {
-        String email = "email@email.com";
+        var email = "email@email.com";
 
         Mockito.when(this.dynamoDb.load(Key.builder().partitionValue(email).build(), Customer.class)).thenReturn(null);
 
         Assertions.assertThrows(CustomerNotFoundException.class, () -> this.service.profile(email));
+    }
+
+    @Test
+    @DisplayName("Should be possible update customer data")
+    void should_be_possible_update_customer_data() {
+        var email = "email@email.com";
+
+        var body = new UpdateCustomer(
+                "updated name",
+                "updated description",
+                LocalDate.now().minusYears(30)
+        );
+
+        Mockito.when(this.dynamoDb.load(
+                Key.builder().partitionValue(email).build(), Customer.class
+        )).thenReturn(new Customer());
+
+        Assertions.assertDoesNotThrow(() -> this.service.update(email, body));
+    }
+
+    @Test
+    @DisplayName("Should not be possible update customer when customer not found")
+    void should_not_be_possible_update_customer_when_customer_not_found() {
+        var email = "email@email.com";
+
+        var body = new UpdateCustomer(
+                "updated name",
+                "updated description",
+                LocalDate.now().minusYears(30)
+        );
+
+        Mockito.when(this.dynamoDb.load(Key.builder().partitionValue(email).build(), Customer.class)).thenReturn(null);
+
+        Assertions.assertThrows(CustomerNotFoundException.class, () -> this.service.update(email, body));
     }
 }
