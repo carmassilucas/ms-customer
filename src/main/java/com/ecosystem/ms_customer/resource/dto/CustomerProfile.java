5190c323c1e3e0ce6b73f4f6686733fe391678ff
Lucas Carmassi <carmassilucas@gmail.com>
2025-03-22 00:39:15 -0300

    refactor: removing password from customer profile response

--- src/main/java/com/ecosystem/ms_customer/resource/dto/CustomerProfile.java
+++ src/main/java/com/ecosystem/ms_customer/resource/dto/CustomerProfile.java
@@ -3,7 +3,6 @@ package com.ecosystem.ms_customer.resource.dto;
 import com.ecosystem.ms_customer.entity.Customer;
 
 public record CustomerProfile(String email,
-                              String password,
                               String name,
                               String profilePicture,
                               String description) {
@@ -11,7 +10,6 @@ public record CustomerProfile(String email,
     public static CustomerProfile fromCustomer(Customer customer) {
         return new CustomerProfile(
                 customer.getEmail(),
-                customer.getPassword(),
                 customer.getName(),
                 customer.getProfilePicture(),
                 customer.getDescription()
