9f444eaa187af3ae82b858ff6c0779f5a4432259
Lucas Carmassi <carmassilucas@gmail.com>
2025-04-01 15:34:55 -0300

    feat: adding security dependencies and allowed routes

---
+++ src/main/java/com/ecosystem/ms_customer/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.ecosystem.ms_customer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers(HttpMethod.POST, "/v1/customers").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/v1/customers/auth").permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
